878dbfcddfae4939aa3b9f47abb767fde3c580af
Aanxhel <ang.core92@yahoo.com>
2022-05-29 21:44:24 -0500

    creacion del home view

--- TacosTacones/appdevweb/src/app/app-routing.module.ts
+++ TacosTacones/appdevweb/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'home-view', loadChildren: () => import('./modules/components/home-view/home-view.module').then(m => m.HomeViewModule) }, { path: 'home-view', loadChildren: () => import('./modules/components/home-view/home-view.module').then(m => m.HomeViewModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
